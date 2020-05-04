536758c271c70f45f75984e1b300dcce3b2affc8
Soumya Shekhar <35521070+Soumya-Shekhar@users.noreply.github.com>
2020-05-04 21:02:50 +0530

    Tried to add a password file saver
    Tried to create files for storing Emails and their keys respectively, and showing their content within the app itself so that one can easily find their emails and keys in one place only.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
+from kivy.storage.jsonstore import JsonStore
+from kivy.uix.recycleview import RecycleView
+
+store = JsonStore('file.json')
 
 class grid(GridLayout):
 	pass
@@ -8,19 +12,28 @@ class grid(GridLayout):
 class PasswordScreen(Screen):
 	pass
 
-
 class KeyScreen(Screen):
 	pass
 
 class ErrorScreen(Screen):
 	pass
 
+class RV(RecycleView):
+	pass
 
 class ResultScreen(Screen):
 	pass
 
+class FileScreen(Screen):
+	pass
 
 class sm(ScreenManager):
+	pass
+
+class SaveData(Screen):
+	pass
+
+class DelData(Screen):
 	pass
 
 class Pass_Gen(App,grid):
@@ -53,8 +66,42 @@ class Pass_Gen(App,grid):
 		except:
 			self.pswd = 'Invalid Input!' + " " + "Please Input Integer only!"
 		return self.pswd
-
-
+	def webfil(self,a):
+		self.fw = open("webfile.txt",'w')
+		self.fw.write(a + "\n")
+		self.fw.close()
+	def emailfil(self,a):
+		self.fe = open("emailfile.txt",'w')
+		self.fe.write(a + "\n")
+		self.fe.close()
+	def key1fil(self,a):
+		self.fk1 = open("key1file.txt",'w')
+		self.fk1.write(a + "\n")
+		self.fk1.close()
+	def key2fil(self,a):
+		self.fk2 = open("key2file.txt",'w')
+		self.fk2.write(a + "\n")
+		self.fk2.close()
+	def websav(self):
+		self.sw = open("webfile.txt")
+		self.br = self.sw.read()
+		self.sw.close()
+		return self.br
+	def emailsav(self):
+		self.se = open("emailfile.txt")
+		self.be = self.se.read()
+		self.se.close()
+		return self.be
+	def key1sav(self):
+		self.sk1 = open("key1file.txt")
+		self.bk1 = self.sk1.read()
+		self.sk1.close()
+		return self.bk1
+	def key2sav(self):
+		self.sk2 = open("key2file.txt")
+		self.bk2 = self.sk2.read()
+		self.sk2.close()
+		return self.bk2
 
 if __name__ == "__main__":
 	Pass_Gen().run()
